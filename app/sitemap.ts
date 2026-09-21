c597a8079d72c311c2e7b72e7c521a3675d4897f
Nathi <madlophanathi23@gmail.com>
2026-09-21 23:10:10 +0400

    Add lash services page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -23,6 +23,14 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 0.9,
     },
+
+{
+  url: `${websiteUrl}/lash-services`,
+  lastModified: new Date(),
+  changeFrequency: "weekly",
+  priority: 0.9,
+},
+
     {
       url: `${websiteUrl}/booking`,
       lastModified: new Date(),
